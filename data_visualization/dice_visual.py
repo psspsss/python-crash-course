8c1d86a7962b0078d3017f89c01b9199baadd4b8
vinayak majhi <vinayak.djp@gmail.com>
2025-11-10 12:14:18 +0530

    dice histograms using pygal

---
+++ data_visualization/dice_visual.py
@@ -0,0 +1,32 @@
+import pygal
+from die import Die
+
+
+die1 = Die()
+die2 = Die(10)
+
+results = []
+
+for roll_num in range(1000):
+    result = die1.roll() + die2.roll()
+    results.append(result)
+
+
+frequencies = []
+max_result = die1.num_sides + die2.num_sides
+
+for value in range(2, max_result + 1):
+    freq = results.count(value)
+    frequencies.append(freq)
+
+
+hist = pygal.Bar()
+
+hist.title = "Results of rolling Two D6 dice 1000 times."
+hist.x_labels = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"]
+hist.x_title = "Result"
+hist.x_title = "Frequency of Result"
+
+
+hist.add("D6 + D6", frequencies)
+hist.render_to_file("dice_visual.svg")
